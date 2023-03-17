8983732de14fc39b5329c7b50bcc2460efeb802b
Raghavan M <20pw26@psgtech.ac.in>
2023-03-17 11:06:58 +0530

    app created

---
+++ SpotifyApiClone/src/main/java/com/example/SpotifyApiClone/SpotifyApiCloneApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpotifyApiClone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyApiCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotifyApiCloneApplication.class, args);
+	}
+
+}
